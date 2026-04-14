a5fa516914e4ad5f84522b0a9e2f28d5041bc349
gcgross144-hash <gcgross144@gmail.com>
2026-04-13 22:27:35 -0400

    Update checkbox.tsx

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,3 +1,26 @@
-export function Checkbox({ checked, onCheckedChange }: any) {
-  return <input type="checkbox" checked={checked} onChange={(e) => onCheckedChange?.(e.target.checked)} />;
+import * as React from "react";
+
+type CheckboxProps = {
+  checked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+  disabled?: boolean;
+  id?: string;
+};
+
+export function Checkbox({
+  checked = false,
+  onCheckedChange,
+  disabled = false,
+  id,
+}: CheckboxProps) {
+  return (
+    <input
+      id={id}
+      type="checkbox"
+      checked={checked}
+      disabled={disabled}
+      onChange={(e) => onCheckedChange?.(e.target.checked)}
+      className="mt-1 h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-2 focus:ring-blue-300"
+    />
+  );
 }
